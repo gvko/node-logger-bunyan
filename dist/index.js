09c4792a26861d3b97f0e9f4c7574ab9cc2be2c1
Galin Kostov <gako@esmiley.dk>
2018-10-30 16:09:50 +0100

    PROD-4106 Forgot to compile...

--- dist/index.js
+++ dist/index.js
@@ -34,21 +34,20 @@ function init(serviceName, key) {
     let logLevel = 'debug';
     /*
      * Cases:
-     * (1) If local dev env (NODE_ENV == 'default') -> log only to console, from level 'debug'
+     * (1) If local dev env (NODE_ENV == 'default' or 'development') -> log only to console, from level 'debug'
      *
-     * (2) If deployment env (NODE_ENV != 'default' or != 'test') -> log to Log Server (LogDNA)
+     * (2) If deployment env (NODE_ENV != 'default' or != 'development' or != 'test') -> log to Log Server (LogDNA)
      ** if logging of debug is enabled (DEBUG_ENABLE == 'true') -> log from 'debug' level, otherwise from 'info'
      */
-    if (!['default', 'test'].includes(process.env.NODE_ENV)) {
+    if (!['default', 'development', 'test'].includes(process.env.NODE_ENV)) {
         // it's not local dev env -> case 2
-        const logDnaKey = process.env.LOG_DNA_KEY || key;
-        if (!logDnaKey) {
-            console.log('*** FATAL ERROR: LogDNA ingestion key is not set as environment variable. Exiting now...');
+        if (!key) {
+            console.log('*** FATAL ERROR: Logging service ingestion key is not provided. Exiting now...');
             process.exit(1);
         }
         const logServerStream = {
             stream: new LogDnaBunyan({
-                key: logDnaKey,
+                key,
                 hostname: process.env.ENVIRONMENT || `temp-wrong-env_${process.env.NODE_ENV}`,
                 index_meta: true
             }),
